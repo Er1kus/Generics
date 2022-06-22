cb7ec8ad39320ba1490e6a42f21511d59c892a1d
Er1kus <goryaev86@mail.ru>
2022-06-22 15:40:20 +0300

    elimination of homework comments

--- src/main/java/ru/netology/domain/Ticket.java
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -1,5 +1,7 @@
 package ru.netology.domain;
 
+import java.util.Objects;
+
 public class Ticket implements Comparable<Ticket> {
     private int id;
     private int price;
@@ -49,5 +51,18 @@ public class Ticket implements Comparable<Ticket> {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return id == ticket.id && price == ticket.price && duration == ticket.duration && Objects.equals(departure, ticket.departure) && Objects.equals(arrival, ticket.arrival);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, price, departure, arrival, duration);
     }
 }
